1bdf1c45c7d8b846fa25ae79ba7bec98b788a8fa
prashant-raghu <prashantraghu999@gmail.com>
2020-07-31 21:22:24 +0530

    userEvents

--- AuthService/src/services/kafka.service.js
+++ AuthService/src/services/kafka.service.js
@@ -1,59 +1,37 @@
-// export kafka consumers
-
 const { Kafka } = require('kafkajs')
 const { v4: uuidv4 } = require('uuid');
 const config = require('../environments/index');
 const kafka = new Kafka({
-    clientId: 'faasv2_auth',
+    clientId: config.kafka.clientId,
     brokers: config.kafka.brokers,
 })
 const EventEmitter = require('events');
+let group = 'faasTrialv1';
 
 const producer = kafka.producer();
-const consumer = kafka.consumer({ groupId: 'faasTrialv1' });
+const consumer = kafka.consumer({ groupId: group });
 
-class MyEmitter extends EventEmitter { }
-const myEmitter = new MyEmitter();
+class kafkaEmitter extends EventEmitter { }
+const kafkaEmitterInst = new kafkaEmitter();
 
-// app.get('/', async function (req, res) {
-//     //send an event to a topic, consume that, and respond that as the res of this api
-//     let id = uuidv4();
-//     let toSend = {
-//         id,
-//         message: "Sent to recieve"
-//     }
-//     myEmitter.on('event', (message) => {
-//         if (message.value) {
-//             let msg = message.value.toString();
-//             msg = JSON.parse(msg);
-//             if (msg && msg.id == id) {
-//                 console.log(msg);
-//                 res.status(200).send(msg);
-//             }
-//         }
-//         console.log('an event occurred!');
-//     });
-//     await producer.send({
-//         topic: 'faasTrialv1',
-//         messages: [
-//             { value: JSON.stringify(toSend) },
-//         ],
-//     })
-// });
 function init() {
     producer.connect()
         .then(r => {
-
         })
-
     consumer.connect()
-        .then(r => {
-            return consumer.subscribe({ topic: 'faasTrialv1' });
+        .then(async r => {
+            for (let event of config.kafka.events)
+                await consumer.subscribe({ topic: `${config.service.name}_${event}_req` });
+            return 1;
         })
         .then(r => {
             return consumer.run({
                 eachMessage: async ({ topic, partition, message }) => {
-                    myEmitter.emit('event', message);
+                    let data = {
+                        topic,
+                        message
+                    }
+                    kafkaEmitterInst.emit('kafkaMessage', data);
                 }
             })
         })
@@ -65,5 +43,6 @@ function init() {
         })
 }
 
-exports.Consumer = myEmitter;
+exports.Consumer = kafkaEmitterInst;
+exports.Producer = producer;
 exports.init = init;
