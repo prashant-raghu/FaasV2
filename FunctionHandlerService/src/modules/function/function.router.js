37ebb61608b66cf4d19e466ed1dfb2dad2a04cdd
prashant-raghu <prashantraghu999@gmail.com>
2020-07-31 22:52:56 +0530

    FunctionRouter

---
+++ FunctionHandlerService/src/modules/function/function.router.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const passport = require('passport');
+const auth = require("../../middlewares/auth.middleware")
+const Controller = require('./function.controller');
+
+router.post('/create',auth.authorize, Controller.create);
+router.post('/read',auth.authorize, Controller.read);
+router.post('/update',auth.authorize, Controller.update);
+router.post('/delete',auth.authorize, Controller.remove);
+router.route('/')
+    .get(auth.authorize, Controller.read)
+    .post(auth.authorize,Controller.create)
+    .put(auth.authorize, Controller.update)
+    .delete(auth.authorize, Controller.remove)
+
+module.exports = router;
