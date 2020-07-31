16394e3b9bb8c0fd0326c2ba8bda6be61ce53c62
prashant-raghu <prashantraghu999@gmail.com>
2020-07-31 21:21:47 +0530

    trimming

--- AuthService/src/services/passport.service.js
+++ AuthService/src/services/passport.service.js
@@ -4,15 +4,9 @@ const
     JwtStrategy = require('passport-jwt').Strategy,
     ExtractJwt = require('passport-jwt').ExtractJwt,
     AnonymousStrategy = require('passport-anonymous').Strategy,
-    FacebookStrategy = require('passport-facebook-token'),
-    AppleStrategy = require('passport-apple');
-
 
 User = require('../modules/user/user.model.js')
 Config = require('../environments/index');
-
-const CustomGoogleStrategy = require('passport-custom').Strategy
-const { OAuth2Client } = require('google-auth-library')
 
 const ConfigPassport = Config.passport
 const opts = {
