379cebfcf50995ebc36df7cb45316244bc316b5d
prashant-raghu <prashantraghu999@gmail.com>
2020-07-31 22:52:45 +0530

    FunctionHandler

---
+++ FunctionHandlerService/src/modules/function/function.handler.js
@@ -0,0 +1,123 @@
+const sequelize = require('sequelize');
+const Config = require('../../environments/index');
+const Function = require('./function.model');
+async function create(req) {
+    const _b = req.body;
+    try {
+        if (!_b.code) {
+            throw new Error("code cannot be null");
+        } else if (!_b.language) {
+            throw new Error("language cannot be null");
+        } else {
+            data = await Function.create({
+                code: _b.code,
+                language: _b.language,
+                userId: _b.userId
+            })
+            return {
+                code: 200,
+                data: {
+                    status: true,
+                    data: data
+                }
+            }
+        }
+    }
+    catch (err) {
+        console.error(err);
+        return {
+            code: 400,
+            data: {
+                status: false,
+                message: err.message
+            }
+        }
+    }
+};
+
+async function read(req) {
+    const _b = req.body;
+    try {
+        const opts = { where: {} };
+        if (+_b.offset) opts.offset = +_b.offset;
+        if (+_b.limit) opts.limit = +_b.limit;
+        if (_b.userId) opts.where.userId = _b.userId;
+        let u = await Function.findAll(opts)
+        if (!u) {
+            return {
+                code: 400,
+                data: {
+                    status: false,
+                    message: 'functions not found'
+                }
+            }
+        } else {
+            return {
+                code: 200,
+                data: {
+                    status: true,
+                    data: u
+                }
+            }
+        }
+    }
+    catch (err) {
+        console.log(err)
+        return {
+            code: 400,
+            data: { status: true, message: err.message }
+        }
+    }
+};
+
+async function remove(req) {
+    const _b = req.body;
+    try {
+        let u = await Function.destroy({
+            where: {
+                id: _b.functionId,
+            }
+        })
+        return {
+            code: 200,
+            data: { ...u.dataValues, status: true }
+        }
+    }
+    catch (err) {
+        console.log(err)
+        return {
+            code: 400,
+            data: { status: true, message: err.message }
+        }
+    }
+};
+
+async function update(req) {
+    const _b = req.body;
+    try {
+        let u = await Function.update({
+            code: _b.code,
+            language: _b.language,
+        }, {
+            where: {
+                id: _b.functionId,
+                userId: _b.userId
+            }
+        })
+        return {
+            code: 200,
+            data: { ...u.dataValues, status: true }
+        }
+    }
+    catch (err) {
+        console.log(err)
+        return {
+            code: 400,
+            data: { status: true, message: err.message }
+        }
+    }
+}
+exports.create = create;
+exports.read = read;
+exports.remove = remove;
+exports.update = update;
