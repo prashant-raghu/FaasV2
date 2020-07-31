437897df5563ac6d49a3664a58efa001088affff
prashant-raghu <prashantraghu999@gmail.com>
2020-07-31 22:52:27 +0530

    functionController

---
+++ FunctionHandlerService/src/modules/function/function.controller.js
@@ -0,0 +1,23 @@
+const handler = require("./function.handler")
+exports.create = async (req, res) => {
+    const _b = req.body;
+    let ret = await handler.create(req)
+    res.status(ret.code).send(ret.data)
+};
+exports.read = async (req, res) => {
+    const _b = req.body;
+    let ret = await handler.read(req)
+    res.status(ret.code).send(ret.data)
+};
+
+exports.remove = async (req, res) => {
+    const _b = req.body;
+    let ret = await handler.remove(req)
+    res.status(ret.code).send(ret.data)
+};
+
+exports.update = async (req, res) => {
+    const _b = req.body;
+    let ret = await handler.update(req)
+    res.status(ret.code).send(ret.data)
+};
